a54311ca2b8c51ed7224d4004f01d11554734d42
Pratik Chakraborty <93854742+pratikc192@users.noreply.github.com>
2022-12-20 07:04:22 +0000

    Deletes backup.js

--- backup.js
+++
@@ -1,8 +0,0 @@
-/*var cityTimezones = require('city-timezones');
-const cityLookup = cityTimezones.lookupViaCity('Larkspur CO');
-console.log(cityLookup);*/
-
-const WorldCities = require('worldcities');
-const city = WorldCities.getByName('Cohasset');
-console.log(city);
-
